84659c0a843e2348e89519c0e9499018e5b9f2a1
Babai <Babai@Babai_PC>
2018-08-27 22:37:32 +0530

    3rd Commit

--- connectstobq/src/main/java/Connect_to_BQ/connectstobq/Main_Class.java
+++ connectstobq/src/main/java/Connect_to_BQ/connectstobq/Main_Class.java
@@ -16,7 +16,7 @@ import java.util.UUID;
  * Hello world!
  *
  */
-public class App 
+public class Main_Class 
 {
   public static void main(String... args) throws Exception {
     BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
--- connectstobq/src/test/java/Connect_to_BQ/connectstobq/Main_ClassTest.java
+++ connectstobq/src/test/java/Connect_to_BQ/connectstobq/Main_ClassTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class Main_ClassTest 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public Main_ClassTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( Main_ClassTest.class );
     }
 
     /**
